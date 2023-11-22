54914d34a378b1d11884abc06cb53a6f673d82bf
Asadullah <asad5918@gmail.com>
2023-11-23 00:10:43 +0530

    First commit

---
+++ Health Care/Model/DetailModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailModel.swift
+//  Health Care
+//
+//  Created by Md Asadullah on 22/11/23.
+//
+
+import Foundation
---
+++ Health Care/Model/ListModel.swift
@@ -0,0 +1,33 @@
+//
+//  ListModel.swift
+//  Health Care
+//
+//  Created by Md Asadullah on 22/11/23.
+//
+
+import Foundation
+
+struct ApiResponse: Codable {
+    let Result: Result
+}
+
+struct Result: Codable {
+    let Error: String
+    let Total: Int
+    let Language: String
+    let Items: Items
+}
+
+struct Items: Codable {
+    let Item: [Item]
+}
+
+struct Item: Codable {
+    let Id: String
+    let Title: String
+
+    enum CodingKeys: String, CodingKey {
+        case Id = "Id"
+        case Title = "Title"
+    }
+}
